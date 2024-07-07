1b346669eae47daf1ef76c1eb7e9d4708242a76c
prosper <edwardprosper001@gmail.com>
2024-07-07 05:54:48 +0100

    'updates'

--- web/urls.py
+++ web/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import staff_login, home, dashboard, customer_login
+from .views import staff_login, customer_signup, home, dashboard, customer_login
 
 
 urlpatterns = [
     path("login/customer/", customer_login, name="login"),
+    path("signup/customer/", customer_signup, name='customer-signup'),
     path('login/staff/', staff_login, name='staff-login'),
     path("dashboard/", dashboard, name="dashboard"),
     path("home/", home, name="home")
--- web/views.py
+++ web/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.contrib import messages
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, get_user_model
 from .models import Contact
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+
+User = get_user_model()
 
 def staff_login(request):
     if request.method == 'POST':
@@ -39,10 +41,34 @@ def customer_login(request):
             messages.error(request, "Invalid username or password.")
     else:
         return render(request, 'web/login.html')
-           
-
-        
     return render(request, 'web/login.html')
+
+
+def customer_signup(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email_address')
+        password = request.POST.get('password')
+        password2 = request.POST.get("password2")
+
+        if password == password2:
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'Username is already taken.')
+                return redirect('signup')
+            elif User.objects.filter(email=email).exists():
+                messages.error(request, 'Email is already registered.')
+                return redirect('signup')
+            else:
+                User.objects.create_user(username=username, email=email, password=password)
+                messages.success(request, "Account creation successful!")
+                return redirect(reverse('login'))
+        else:
+            messages.error(request, "Both passwords must match!")
+            return redirect(reverse('login'))
+
+    return render(request, 'web/customer_signup.html')
+
+
 
 
 @login_required
