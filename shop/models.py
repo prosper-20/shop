a6c6d7864fc8c0bbcd1d9508d48fe93908b9cca5
prosper <edwardprosper001@gmail.com>
2024-07-30 09:00:51 +0100

    'updates'

--- shop/models.py
+++ shop/models.py
@@ -30,8 +30,12 @@ class Shop(models.Model):
         ('vacant', 'Vacant'),
         ('allocated', 'Allocated'),
      
-       
     ]
+
+    APPROVAL_STATUS = (
+        ('In review', 'In review'),
+        ('Pending Approval', 'Pending Approval'),
+    )
     name = models.CharField(max_length=255)
     type = models.CharField(max_length=10, choices=Type)
     price = models.DecimalField(max_digits=10, decimal_places=2)
@@ -41,6 +45,7 @@ class Shop(models.Model):
     size = models.IntegerField()
     status = models.CharField(max_length=10, choices=STATUS, default='vacant')
     is_paid = models.BooleanField(default=False)
+    approval = models.CharField(max_length=100, choices=APPROVAL_STATUS)
 
     def __str__(self):
         return f"Shop {self.no}"
