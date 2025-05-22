a0a451bb929070a8c3f6f83429ca4e74482c9e3f
prosper <edwardprosper001@gmail.com>
2025-05-22 17:21:43 +0100

    fix: resolved rent reminder signals

--- shop/signals.py
+++ shop/signals.py
@@ -196,13 +196,6 @@ def update_shop_is_not_paid_again(sender, instance, **kwargs):
 
 @receiver(post_save, sender=Shop)
 def send_email_on_shop_creation(sender, instance, created, **kwargs):
-    # if created:
-    #     subject = 'New Shop Created'
-    #     message = f'Hello sir, a new shop named {instance.no} has been created, Kindly review and approve it!'
-    #     sender = settings.EMAIL_HOST_USER
-    #     recipient = [user.email for user in User.objects.all() if user.is_superuser==True]
-    #     send_mail(subject, message, sender, recipient, fail_silently=False)
-
     if created:
         subject = "New Shop Created"
         html_message = render_to_string("shop/new_shop_email.html", {"shop": instance})
@@ -217,19 +210,27 @@ def send_email_on_shop_creation(sender, instance, created, **kwargs):
 
 @receiver(post_save, sender=Rent)
 def send_rent_creation_email(sender, instance, created, **kwargs):
+    # if created:
+    #     subject = 'Rent Paid !!'
+    #     message = f'Dear {instance.customer.name},\n\n' \
+    #               f'Your rent for the shop {instance.shop.no} has been created.\n' \
+    #               f'Rent Type: {instance.rent_type}\n' \
+    #               f'Rent Start Date: {instance.rent_start}\n' \
+    #               f'Due Date: {instance.date_due}\n\n' \
+    #               f'Thank you for your business!\n' \
+    #               f'Best regards,\n' \
+    #               f'Nina Sky Innovation Limited'
+    #     sender = settings.EMAIL_HOST_USER
+    #     recipient_list = [instance.customer.email]
+    #     send_mail(subject, message, sender, recipient_list, fail_silently=False)
+
     if created:
-        subject = 'Rent Paid !!'
-        message = f'Dear {instance.customer.name},\n\n' \
-                  f'Your rent for the shop {instance.shop.no} has been created.\n' \
-                  f'Rent Type: {instance.rent_type}\n' \
-                  f'Rent Start Date: {instance.rent_start}\n' \
-                  f'Due Date: {instance.date_due}\n\n' \
-                  f'Thank you for your business!\n' \
-                  f'Best regards,\n' \
-                  f'Nina Sky Innovation Limited'
-        sender = settings.EMAIL_HOST_USER
-        recipient_list = [instance.customer.email]
-        send_mail(subject, message, sender, recipient_list, fail_silently=False)
+        subject = "Shop Allocation Confirmation"
+        html_message = render_to_string("rent/rent_allocation_email.html", {"shop": instance.shop, "rent": instance, "customer": instance.customer})
+        plain_message = strip_tags(html_message)
+        from_email = config("EMAIL_HOST_USER")
+        to = [instance.customer.email]
+        send_mail(subject, plain_message, from_email, to, html_message=html_message)
 
 
 
