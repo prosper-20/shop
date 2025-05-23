a2654b134f5fdfb287ab4cd44a8e8fd6e04f9d81
prosper <edwardprosper001@gmail.com>
2025-05-23 13:13:47 +0100

    fix: resolved function call failure

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-l@+f$1d&75+jjo)+*c1w0)t4w1!nl@2&l_h_&4h0jd&*x1%s-)"
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
