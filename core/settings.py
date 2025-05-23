497deecd0e1d5c18eaae870420e1527d9ff935e6
prosper <edwardprosper001@gmail.com>
2025-05-23 13:17:38 +0100

    fix: resolved function call failure

--- core/settings.py
+++ core/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["ninasky.onrender.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["nina-sky.onrender.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
